8a80ccd92fc87a084bf36ec00c1ab56ae1ff9b54
hs.Lee <hs.lee@ukmedia.co.kr>
2024-05-02 21:46:24 +0900

    feat : 프로그램 중복실행 방지 기능 추가

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace PadControlHelper {
     internal static class Program {
         /// <summary>
@@ -5,10 +7,20 @@ namespace PadControlHelper {
         /// </summary>
         [STAThread]
         static void Main() {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            if(IsExistProcessMutex(Process.GetCurrentProcess().ProcessName)) {
+                MessageBox.Show("LeeBaeng PCH가 이미 실행 중입니다.", "중복 실행 불가");
+            } else {
+                ApplicationConfiguration.Initialize();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            //Application.Run(new Form1());
+        }
+
+        private static bool IsExistProcessMutex(string processName) {
+            new Mutex(true, processName, out bool createdNew);
+            return !createdNew;
         }
     }
 }
